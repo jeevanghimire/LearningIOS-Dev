5840f07dfb082bf918ad51c5933b174dd967c145
Jeevan Ghimire <imjeevanghimire@gmail.com>
2024-01-20 20:22:43 +0545

    Initial Commit

---
+++ LearningIOS Dev/LearningIOS_DevApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningIOS_DevApp.swift
+//  LearningIOS Dev
+//
+//  Created by Jeevan Ghimire on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningIOS_DevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
