58f0840e438c06243caeca52ced77e6527eced1d
Jeevan Ghimire <imjeevanghimire@gmail.com>
2024-06-02 11:33:29 +0545

    all being all

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jeevan Ghimire on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
